2bb9e582f8d28e052406bf4a39022fd00e4230a5
adrian <adrian.radillo@gmail.com>
2018-05-10 22:22:49 -0500

    parallel script for single dataset on server

--- decide.m
+++ decide.m
@@ -1,38 +1,37 @@
-%############ VALIDATED
-% display('single epoch')
-% tic
-% v = end_point_nonlin(0.7, .5, 1);
-% toc
-%tic
-%for i = 1:10000
-%    v = end_point_nonlin(i/10000,i/9900,1);
-%end
-%toc
+%{
+	Content of srvr_data_1.h5
+	--------------------------
+	lr15hr101.258639865h1T2
+	lr15hr101.258639865h1T2/decision_lin
+	lr15hr101.258639865h1T2/decision_nonlin
+	lr15hr101.258639865h1T2/trial_info
+	lr15hr101.258639865h1T2/trials
+	lr15hr17.8664640249h1T2
+	lr15hr17.8664640249h1T2/decision_lin
+	lr15hr17.8664640249h1T2/decision_nonlin
+	lr15hr17.8664640249h1T2/trial_info
+	lr15hr17.8664640249h1T2/trials
+	lr15hr36.5367250374h1T2
+	lr15hr36.5367250374h1T2/decision_lin
+	lr15hr36.5367250374h1T2/decision_nonlin
+	lr15hr36.5367250374h1T2/trial_info
+	lr15hr36.5367250374h1T2/trials
+%}
 
-td = 2;
-% ls = [1/3, pi/6, .8422343];
-% rs = [1/6, 2/3, .75378];
-kappa = log(101.258639865/15);
-hh = 1;
-% display('single trial')
-% tic
-% dd = decide1(td, ls, rs, kappa, hh, 0)
-% toc
 
-% hdf5 DB
-dbname = '/scratch/adrian/sandbox_data.h5';
-dsetname = '/lr15hr101.26h1T2/trials';
-dsetname_decision = '/lr15hr101.26h1T2/decision_nonlin';
-%h5disp(dbname, dsetname)
-
-ntrials=90;
+ntrials=100000;
 ncols=2;
-ncols_write=101;
-trial_data = h5read(dbname,dsetname,[1 1],[ncols ntrials]);
-display('100 trials')
-
+ncols_write=10001;
 all_h = [1, linspace(0,40,ncols_write-1)];
-tic
+td = 2;
+
+% S8 data
+dbname = '/scratch/adrian/srvr_data_1.h5';
+dsetname = '/lr15hr101.258639865h1T2/trials';
+dsetname_decision = '/lr15hr101.258639865h1T2/decision_nonlin';
+trial_data = h5read(dbname,dsetname,[1 1],[ncols ntrials]);
+kappa = log(101.258639865/15);
+
 for i = 1:ntrials
     ls = trial_data{1,i};
     rs = trial_data{2,i};
@@ -44,4 +43,3 @@ for i = 1:ntrials
     count=[ncols_write 1];
     h5write(dbname, dsetname_decision, dec, start, count)
 end
-toc
