dfc26cbaf7a78b9961d4f4c8760775636fb2db67
adrian <adrian.radillo@gmail.com>
2018-05-15 16:44:28 -0500

    particle filter slow but runs on 2 trials

--- part_filter_1.m
+++ part_filter_1.m
@@ -16,19 +16,21 @@ true_h = h5readatt(dbname, info_dset, 'h'); % true hazard rate
 k=log(hr/lr); % kappa for mean jump size in LLR at click
 
 hs=linspace(0,10,10)'; % values of h to try
-ntrials=1;
+ntrials=2;
 npart=100;
 nsd=1;
 ncols=2; %nb of columns in db
 trial_data = h5read(dbname, dsetname, [1 1], [ncols ntrials]);
-lst = trial_data{1,1}; % 2nd idx to change
-rst = trial_data{2,1}; % 2nd idx to change
 log_posterior=zeros(1,length(hs));
 tic
-for num_s = 1:length(hs)
-    synthetic_decision = gauss_noise_nonlin_decide(T, lst, rst, k, true_h, 0, nsd);
-    log_posterior(num_s)=log_posterior(num_s)+...
-        log(lhd_nonlin_sing_tr_gauss_clicks(synthetic_decision, npart, lst, rst, T, k, hs(num_s), 0, nsd));
+for num_trial = 1:ntrials
+    lst = trial_data{1,num_trial}; 
+    rst = trial_data{2,num_trial}; 
+    for num_s = 1:length(hs)
+        synthetic_decision = gauss_noise_nonlin_decide(T, lst, rst, k, true_h, 0, nsd);
+        log_posterior(num_s)=log_posterior(num_s)+...
+            log(lhd_nonlin_sing_tr_gauss_clicks(synthetic_decision, npart, lst, rst, T, k, hs(num_s), 0, nsd));
+    end
 end
 toc
 plot(hs, log_posterior, 'LineWidth', lw)
