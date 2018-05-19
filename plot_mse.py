56e241eee5e6fa791588625c9b4eb6fd2efa9e1f
adrian <adrian.radillo@gmail.com>
2018-05-19 09:09:21 -0500

    finish mse panels

--- plot_mse.py
+++ plot_mse.py
@@ -7,28 +7,28 @@ font = {'family': 'DejaVu Sans',
 lw = 4  # linewidth
 matplotlib.rc('font', **font)
 
-results = pickle.load(open("data/mse_local_semivalid.pkl", "rb"))
-
-# first file
-fnum = 0
-file = results[fnum]['file']
-trial_numbers = file[1]
+results = pickle.load(open("data/MSE_data_srvrs.pkl", "rb"))
 
 row2modl_map = ['linlin', 'nonlinnonlin', 'linnonlin', 'nonlinlin']
-f, ax_array = plt.subplots(4, 1, sharex='all', sharey='col', squeeze=False)
-# loop over rows of suplot
-for r in range(4):
-    # lin 2 lin fit
-    data = results[fnum]['stats'][row2modl_map[r]]
-    # explore failures
-    # fail_dicts = [x[1] for x in data]
 
-    # MSE plot
-    ax = ax_array[r, 0]
-    ax.plot(trial_numbers, [x[0] for x in data], linewidth=lw)
-    ax.set_xlabel('trial nb')
-    ax.set_ylabel('scalar error')
-    ax.set_title(row2modl_map[r])
+for fnum in range(2):
+    if fnum == 0:
+        continue
+    file = results[fnum]['file']
+    trial_numbers = file[1]
+    f, ax_array = plt.subplots(4, 1, sharex='all', sharey='col', squeeze=False)
+    # loop over rows of suplot
+    for r in range(4):
+        # lin 2 lin fit
+        data = results[fnum]['stats'][row2modl_map[r]]
+        # explore failures
+        # fail_dicts = [x[1] for x in data]
 
-plt.tight_layout()
-plt.show()
+        # MSE plot
+        ax = ax_array[r, 0]
+        ax.plot(trial_numbers, [x[0] for x in data], 'o--', markersize=10, linewidth=lw)
+        ax.set_xlabel('trial nb')
+        ax.set_ylabel('scalar error')
+        ax.set_title(row2modl_map[r])
+    plt.tight_layout()
+    plt.show()
