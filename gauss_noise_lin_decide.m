732e5dc7a67e00253aeeca244dea63c6c266f4fd
adrian <adrian.radillo@gmail.com>
2018-05-31 18:53:32 -0500

    remove seed from gauss_noise_lin_decide.m

--- gauss_noise_lin_decide.m
+++ gauss_noise_lin_decide.m
@@ -1,7 +1,6 @@
-function decision = gauss_noise_lin_decide(left_train, right_train, gamma, kappa, noise_sd, seed, y0)
+function decision = gauss_noise_lin_decide(left_train, right_train, gamma, kappa, noise_sd, y0)
 % returns decision, -1,0 or 1, using the linear model with Gaussian
 % multiplicative noise applied to clicks
-rng(seed);
 noise_left_train = normrnd(kappa, noise_sd, size(left_train));
 noise_right_train = normrnd(kappa, noise_sd, size(right_train));
 yp=sum(noise_right_train.*exp(gamma*right_train));
