27307df2ec35e4ba49987c544c043696564e7592
adrian <adrian.radillo@gmail.com>
2018-06-17 10:49:23 -0500

    acc curves S3lr2h1T2tr5Ksp10K.h5

--- Python3_code/get_accuracy.py
+++ Python3_code/get_accuracy.py
@@ -15,20 +15,23 @@ if __name__ == '__main__':
             splecol = 0
         print(get_accuracy(filename, grpname, model, splecol))
     else:
-        fname = "../data/S3lr5h1T2tr10000sp1000.h5"
-        gname = "lr5hr20h1T2"
+        # fname = "../data/S3lr5h1T2tr10000sp1000.h5"
+        fname = "../data/S3lr2h1T2tr5Ksp10K.h5"
+        # gname = "lr5hr20h1T2"
+        gname = "lr2hr14h1T2"
+        # ncols = 1000
+        ncols = 10000
         models = ('lin', 'nonlin')
-        ncols = 1000
-        accuracies = np.zeros((2, ncols))
+        accuracies = np.zeros((2, ncols))  # row1=nonlin; row0=lin
         for col in np.arange(ncols):
             accuracies[0, col] = get_accuracy(fname, gname, models[0], sample_col=col+1)
             accuracies[1, col] = get_accuracy(fname, gname, models[1], sample_col=col+1)
 
         plt.plot(np.linspace(0, 10, ncols), accuracies[1, :],
-                 np.linspace(0, 10, ncols)/get_best_gamma(3, 1, polyfit=False), accuracies[0, :],
+                 np.linspace(0, 40, ncols)/get_best_gamma(3, 1, polyfit=False), accuracies[0, :],
                  linewidth=3)
-        plt.plot([1, 1], [.65, .9])
+        plt.plot([1, 1], [.5, 1])
         plt.ylabel('% correct')
         plt.xlabel('relative param')
-        plt.legend(['nonlin', 'lin','true'])
+        plt.legend(['nonlin', 'lin', 'true'])
         plt.show()
