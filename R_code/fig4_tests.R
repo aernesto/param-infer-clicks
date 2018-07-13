1a5ad511f38b5070a95664152777369f4ab42534
adrian <adrian.radillo@gmail.com>
2018-07-13 08:38:10 -0500

    fig4 edits

--- R_code/fig4_tests.R
+++ R_code/fig4_tests.R
@@ -3,15 +3,16 @@ library(ggplot2)
 fig4_data <- read.csv(file="../data/fig4_data.csv", header=TRUE, sep=",")
 
 gg <- ggplot(fig4_data,aes(x=trial_nb,y=error)) +
-             geom_point(aes(col=model_pair, shape=method),size=4) +
+             geom_point(aes(col=model_pair, shape=method),size=6) +
              labs(title="Error as fcn of block size", 
-                  subtitle="Iteration 1", y="Relative Error", 
+                  y="Relative Error", 
                   x="Block Size (in trials)") +
              scale_colour_brewer(palette = "Set1") # change color palette
 #gg + facet_wrap(method~model_pair, nrow = 2)
-plot(gg + theme(text = element_text(size=20)) + 
+plot(gg + theme(text = element_text(size=30)) + 
   facet_grid(method~model_pair) + 
-    scale_x_discrete(expand=c(0.1, 0.1), 
+    scale_x_discrete(expand=c(0.1, 0.1),
+                     labels=c("","100","300","","500"),
                      limits=unique(fig4_data$trial_nb)))
   
 #gg + theme_bw()  # change whole theme
