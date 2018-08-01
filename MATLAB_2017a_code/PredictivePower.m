ba86e25bd9b0ac70786ff59cd04b1a256916444a
adrian <adrian.radillo@gmail.com>
2018-08-01 18:32:03 -0500

    first computation of PP depends on bin width

--- MATLAB_2017a_code/PredictivePower.m
+++ MATLAB_2017a_code/PredictivePower.m
@@ -1,11 +1,22 @@
-% assess the distribution of the Bernoulli parameter q across clicks trials
-% Parameter q is defined as follows: it is the probability that the
-% decision maker answers 1, given the clicks stimulus
-% I assume all clicks stimuli have equal probability and only consider the
-% histogram of q-values
+% compute predictive power according to the methodology described below:
+% https://paper.dropbox.com/doc/Stochastic-model-fitting-URuXW5PKABizdbMJB4Bkb#:uid=862179754392750879685852&h2=Definition-of-predictive-power
+
+% For now, only focus on L-L, with m_f and m_d using true discounting param
+
 clear
 
-%1. -------------Get a bank of clicks data---------------------------------
+%1. -----------bin q------------------------------------------------------%
+
+nbins=100;  % actual number of bins for histogram and density
+qbins=linspace(0,1,nbins+1);  % really bin edges as needed by histogram()
+qvalues=zeros(1,nbins); % mid-points of bins
+for i=1:nbins
+    qvalues(i)=(qbins(i+1)-qbins(i))/2;
+end
+    
+
+%2. -----------get density of q across trials-----------------------------%
+%%%2a. -------------Get a bank of clicks data-----------------------------%
 
 filename = '../data/S3lr5h1T2tr10000sp1000.h5';%'../data/validation1.h5';
 file_info = h5info(filename);
@@ -30,10 +41,10 @@ all_trials = h5read(filename, [group_name,'/trials']);
 % [left_train, right_train] = all_trials{1:2,50};  
 
 
-%2. -------------For each trial, compute the q-value-----------------------
+%%%2b. -------------For each trial, compute the q-value-------------------%
 
 k=log(high_rate/low_rate);
-nsd=2; % Gaussian noise applied to click height
+nsd=1; % Gaussian noise applied to click height
 q=zeros(1,tot_num_trials); % store q-values
 
 for trn=1:tot_num_trials
@@ -42,4 +53,18 @@ for trn=1:tot_num_trials
     q(trn)=exp(lhd_lin_sing_tr_gauss_clicks(1, nsd, k,...
     T, lst', rst', true_g));
 end
-hist=histogram(q,'Normalization','pdf');
+hist=histogram(q,'Normalization','pdf',...
+    'NumBins',nbins,...
+    'BinEdges',qbins);
+
+q_density=hist.Values;
+
+%3. --------get probability of agreement as fcn of q----------------------%
+
+agreement=qvalues.^2+(1-qvalues).^2;
+
+%4. ----------------compute predictive power------------------------------%
+
+bin_width=1/nbins; % equivalently bin_width=hist.BinWidth;
+
+PP=bin_width*sum(agreement.*q_density)
