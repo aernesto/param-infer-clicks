979e2871b7dbcdf92af843e30dac09674322c1c9
adrian <adrian.radillo@gmail.com>
2018-08-07 18:50:12 -0500

    prepare launch of joint pp

--- MATLAB_2017a_code/cross_param_PP_L_NL.m
+++ MATLAB_2017a_code/cross_param_PP_L_NL.m
@@ -5,9 +5,9 @@ clear
 tic
 rng('shuffle')
 nsd=1; % noise
-ntrials=10;%100000
-gammas=0:0.05:20; num_gammas=length(gammas); 
-hs=0:0.01:5; num_h=length(hs);
+ntrials=1000;
+gammas=0:0.1:10; num_gammas=length(gammas); 
+hs=0:0.1:2.5; num_h=length(hs);
 num_pairs=num_gammas*num_h;
 
 pairs=zeros(num_pairs,2);
@@ -22,29 +22,45 @@ end
 
 db = '../data/validation2.h5';%'/home/adrian/validation1.h5';
 [trials,envt]=get_trials(db,ntrials);
+% remove problematic trials
+%size(trials)
+bad_trials=sort([272270,207048,229626,93737,555142,774387,666886,...
+    811388,961053,631387]);
+trials(:,bad_trials)=[];
+left_clicks=trials(1,:);
+right_clicks=trials(2,:);
+ntrials=ntrials-length(bad_trials);
+%size(trials)
 high_rate=20; low_rate=5; k=log(high_rate/low_rate);
 
-
-% ---------------------- end of sanity check -----------------------------%
-PP=zeros(num_gammas);
-for idx1=1:num_gammas
-    for idx2=idx1:num_gammas
-        g1=gammas(idx1); g2=gammas(idx2);
-        match_count=0;
-        for trn=1:tot_num_trials
-            [lst, rst]=all_trials{1:2,trn};
-            
-            dec_1 = gauss_noise_lin_decide(lst, rst, g1, k, nsd, 0);
-            if dec_1 == 0; dec_1 = sign(rand-0.05); end  % flip a coin if 0
-            
-            dec_2 = gauss_noise_lin_decide(lst, rst, g2, k, nsd, 0);
-            if dec_2 == 0; dec_2 = sign(rand-0.05); end
-            
-            if dec_1==dec_2
-                match_count=match_count+1;
-            end
+PP=zeros(num_h,num_gammas);
+for num_pair=1:num_pairs
+    g=pairs(num_pair,1); h=pairs(num_pair,2);
+    match_count=0;
+    parfor trn=1:ntrials
+        lst=left_clicks{trn}; rst=right_clicks{trn};
+%         [lst, rst]=trials{1:2,trn};
+        
+        total_clicks = length(lst)+length(rst);
+        
+        % generate decisions with stoch nonlinear model
+        dec_h = decide_AR(2,lst,rst,NaN,h,0,NaN,...
+            normrnd(k, nsd, [total_clicks, 1]));
+        
+        % generate decisions with stoch linear model
+        dec_g = gauss_noise_lin_decide(lst, rst, g, k, nsd, 0);
+        
+        % flip a coin if any decision was 0
+        if dec_h == 0; dec_h = sign(rand-0.05); end
+        if dec_g == 0; dec_g = sign(rand-0.05); end
+        
+        if dec_h==dec_g
+            match_count=match_count+1;
         end
-        PP(idx1,idx2)=match_count/tot_num_trials;
-    end  
+    end
+    PP(idx_h,idx_g)=match_count/ntrials;
 end
 toc
+savefile=['/home/adrian/joint_PP_ntrials_',num2str(ntrials),...
+    '_noise_',num2str(nsd),'.mat'];
+save(savefile,'PP','gammas','hs','ntrials')
