d6536f49484b032625347127e9fc6d7458409690
adrian <adrian.radillo@gmail.com>
2018-08-07 15:07:36 -0500

    try to fix NaN pb

--- MATLAB_2017a_code/compute_acc_L_range_gammas.m
+++ MATLAB_2017a_code/compute_acc_L_range_gammas.m
@@ -1,11 +1,22 @@
 % compute acc of L model for a range of gamma values on validation2.h5
 clear
+
+server_version = true;
+
 tic
+
 rng('shuffle')
-parpool([12,80])
-ntrials=1000000;%10000;takes 24 sec on laptop
+
+if server_version
+    parpool([12,80])
+    ntrials=1000000;
+    db='/scratch/adrian/validation2.h5';
+else
+    ntrials=10000;
+    db='../data/validation2.h5';
+end
+
 gammas=0:.05:20; num_gammas=length(gammas);
-db='/scratch/adrian/validation2.h5';%'../data/validation2.h5';
 
 %1. -----------------------get trials-------------------------------------%
 
@@ -16,6 +27,7 @@ high_rate=20; low_rate=5; k=log(high_rate/low_rate);
 
 Correct= zeros(ntrials,num_gammas);
 nsd = 1; % noise
+nan_trial_count=0;
 parfor trn=1:ntrials
     [lst,rst]=trials{:,trn};
     
@@ -35,15 +47,26 @@ parfor trn=1:ntrials
     
     dec = sign(sum(right_noise.*exp(rst*gammas),1)...
                     -sum(left_noise.*exp(lst*gammas),1));
-    num_zeros = length(find(~dec));
-    dec(dec==0)=randsample([-1,1],num_zeros,true);
-
-    Correct(trn, :) = dec == envt(2,trn);
+    % debug
+    num_dec_nan=sum(isnan(dec));
+    if num_dec_nan
+        nan_trial_count=nan_trial_count+1;
+        fprintf('trial %d, dec_nan=%d\n',trn, num_dec_nan)
+    else
+        num_zeros = length(find(~dec));
+        dec(dec==0)=randsample([-1,1],num_zeros,true);
+        Correct(trn, :) = dec == envt(2,trn);
+    end
 end
-acc = sum(Correct,1)/ntrials;
+fprintf('total of %d NaN trials\n',nan_trial_count)
+acc = sum(Correct,1)/(ntrials-nan_trial_count);
 
 %3. -----------------store and save accuracy------------------------------% 
 %plot(gammas,acc)
-savefile='/home/adrian/acc_L_range_gammas.mat';
-save(savefile,'acc','gammas')
+if server_version
+    savefile='/home/adrian/acc_L_range_gammas.mat';
+    save(savefile,'acc','gammas')
+else
+    plot(gammas,acc)
+end
 toc
