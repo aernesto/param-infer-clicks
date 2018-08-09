a970f0ea05547ba9b013ba0110d10c3a930cc9d3
adrian <adrian.radillo@gmail.com>
2018-08-09 09:57:57 -0500

    test surfaces work

--- MATLAB_2017a_code/surf_joint_PP.m
+++ MATLAB_2017a_code/surf_joint_PP.m
@@ -1,3 +1,4 @@
+%% NL-L
 clear
 load('../data/joint_PP_ntrials_99999_noise_1.mat')
 [X,Y]=meshgrid(gammas,hs);
@@ -7,7 +8,6 @@ ylabel('h')
 zlabel('predictive power')
 % best gamma = 5.65
 % best h = 0.45
-
 
 [A,B]=max(PP);
 hmax=zeros(size(B));
@@ -28,3 +28,23 @@ subplot(2,1,2)
 plot(hs,gmax)
 xlabel('h')
 ylabel('\gamma_{max}')
+
+%% L-L
+clear
+load('../data/joint_PP_LL_ntrials_100_noise_1.mat')
+[X,Y]=meshgrid(thetas_1,thetas_2);
+%num_gammas=length(thetas_1);
+surf(X,Y,PP)
+xlabel('gamma')
+ylabel('h')
+zlabel('predictive power')
+
+%% NL-NL
+clear
+load('../data/joint_PP_NLNL_ntrials_100_noise_1.mat')
+[X,Y]=meshgrid(thetas_1,thetas_2);
+%num_gammas=length(thetas_1);
+surf(X,Y,PP)
+xlabel('gamma')
+ylabel('h')
+zlabel('predictive power')
